2ed9a085822e5260af6d028bd25b5bd9f4fbdf27
yamage.mail@gmail.com <y443525s>
2023-03-14 20:54:32 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # Docker-Db2_SSLconnect-encryptDB
 DockerHubイメージからSSL通信と透過的暗号DBを設定コンテナ作成
+
+#Docker Hub URL: https://hub.docker.com/r/ibmcom/db2
+#Db2バージョン: V11.5.6.0(v11.5 Mod6FP0)
+#※バージョン変更したい場合はDockerfileのFROMセクションに記載のタグのバージョンを修正してください
+#　指定できるバージョンは上記Hub URLを参照
+#前提:
+#・WSL2環境のDockerを使用
+#・Docker for Linux(Community Edition), docker-composeが導入済である
+#・WSL2ログインユーザーがdocker-composeコマンドをroot権限で実行できる
+#・docker-compose.yamlファイルがカレントディレクトリ(移動済)である
+
+#下記ファイルがあること
+# Dockerfile
+# docker-compose.yaml
+# db2.env
+ls -ltr
+
+##image構築
+# "Creating <comtainer_name> ... done"が最後に表示されることを確認
+sudo docker-compose -f ./docker-compose.yaml up -d --build
+
+##コンテナ作成・起動
+# "<comtainer_name> is up-to-date"が表示されることを確認
+sudo docker-compose -f ./docker-compose.yaml up -d
+
+##コンテナ作成ログ確認
+# ※内部でインスタンス作成,DB作成,暗号設定ジョブが動いているので実行ログを確認して完了することを確認
+sudo docker-compose -f ./docker-compose.yaml logs -f
+
+##Db2インスタンスログイン
+docker-compose -f ./docker-compose.yaml exec db2 /bin/bash -c "su - tstdbi1"
+
+##環境変数確認
+#下記の通りになってること
+# [i] DB2_DYNAMIC_SSL_LABEL=ON
+# [i] DB2COMM=SSL,TCPIP
+# [g] DB2SYSTEM=<Container_ID>
+db2set -all
+
+##インスタンス設定確認
+db2 get dbm cfg
+
+##暗号通信設定確認
+db2 get dbm cfg | grep SSL
+
+#DB確認
+db2 list db directory
+
+#DB接続
+db2 connect to TSTDB
+
+##DB設定確認
+db2 get db cfg for TSTDB
+
+##透過的暗号化確認
+# "YES"となってること
+db2 get db cfg for TSTDB | grep "Encrypted database"
+
+#テーブル内にデータ(設定情報)があること
+db2 "select * from TABLE(SYSPROC.ADMIN_GET_ENCRYPTION_INFO())"
+
+#DBプロセス切断
+db2 terminate
+
+##Db2インスタンスログアウト
+exit
---
+++ source/addsslandencryptdb.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#暗号通信ポート設定
+cp /etc/services /etc/services.db2ssl
+echo "db2s_${DB2INSTANCE}      50100/tcp" >> /etc/services
+
+#インスタンスプロファイル読み込み
+. /database/config/${DB2INSTANCE}/sqllib/db2profile
+
+#キーストア作成
+mkdir -m 770 /database/.keys
+chgrp db2iadm1 /database/.keys
+export KEYSTOREPASS=$(/opt/ibm/db2/V11.5/gskit/bin/gsk8capicmd_64 -random -create -length 16 -strong)
+echo ${KEYSTOREPASS} > /database/.keys/KeyStore.pass
+/opt/ibm/db2/V11.5/gskit/bin/gsk8capicmd_64 -keydb -create -db /database/.keys/KeyStore.p12 -type pkcs12 -pw "${KEYSTOREPASS}" -stash
+chmod 660 /database/.keys/KeyStore.p12 /database/.keys/KeyStore.sth
+chgrp db2iadm1 /database/.keys/KeyStore.p12 /database/.keys/KeyStore.sth
+
+#暗号通信証明書作成
+/opt/ibm/db2/V11.5/gskit/bin/gsk8capicmd_64 -cert -create -db /database/.keys/KeyStore.p12 -stashed -label "$(uname -n)_cert_$(date +%Y%m%d)" -dn "CN=$(uname -n),OU=test,O=test,L=Minato,ST=Tokyo,C=JP -expire 730 -size 4096 -sigalg SHA512WithRSA"
+
+#Db2環境変数設定
+
+su - ${DB2INSTANCE} << EOF
+db2set -all
+db2set DB2COMM=SSL,TCPIP
+db2set DB2_DYNAMIC_SSL_LABEL=ON -immediate
+db2set -all
+EOF
+
+#DBM設定
+db2 "attach to ${DB2INSTANCE} user ${DB2INSTANCE} using ${DB2INST1_PASSWORD}"
+db2 "get dbm cfg" | grep -i DIAGLEVEL
+db2 "update dbm cfg using DIAGLEVEL 4"
+db2 "get dbm cfg" | grep -i DIAGLEVEL
+db2 "get dbm cfg" | grep -i KEYSTORE_LOCATION
+db2 "update dbm cfg using KEYSTORE_LOCATION /database/.keys/KeyStore.p12"
+db2 "get dbm cfg" | grep -i KEYSTORE_LOCATION
+db2 "get dbm cfg" | grep -i KEYSTORE_TYPE
+db2 "update dbm cfg using KEYSTORE_TYPE PKCS12"
+db2 "get dbm cfg" | grep -i SSL_VERSIONS
+db2 "update dbm cfg using SSL_VERSIONS TLSV12"
+db2 "get dbm cfg" | grep -i SSL_VERSIONS
+db2 "get dbm cfg" | grep -i SSL_SVR_KEYDB
+db2 "update dbm cfg using SSL_SVR_KEYDB /database/.keys/KeyStore.p12"
+db2 "get dbm cfg" | grep -i SSL_SVR_KEYDB
+db2 "get dbm cfg" | grep -i SSL_SVR_STASH
+db2 "update dbm cfg using SSL_SVR_STASH /database/.keys/KeyStore.sth"
+db2 "get dbm cfg" | grep -i SSL_SVR_STASH
+db2 "get dbm cfg" | grep -i SSL_SVR_LABEL
+db2 "update dbm cfg using SSL_SVR_LABEL $(uname -n)_cert_$(date +%Y%m%d)"
+db2 "get dbm cfg" | grep -i SSL_SVR_LABEL
+db2 "get dbm cfg" | grep -i SSL_SVCENAME
+db2 "update dbm cfg using SSL_SVCENAME db2s_${DB2INSTANCE}"
+db2 "get dbm cfg" | grep -i SSL_SVCENAME
+db2 "detach"
+
+#Db2インスタンス再起動
+su - ${DB2INSTANCE} -c "db2stop;ipclean;db2start"
+
+#DB透過的暗号化
+su - ${DB2INSTANCE} << EOF
+db2 "deactivate db $DBNAME"
+db2 "list db directory"
+db2 "drop db $DBNAME"
+db2 "list db directory"
+db2 "restore database $DBNAME from /database/backup taken at $(ls -1tr /database/backup | tail -1 | grep $DBNAME.0.${DB2INSTANCE}.*.001 | awk -F'.' '{print $5}') encrypt without rolling forward"
+db2 "list db directory"
+EOF
